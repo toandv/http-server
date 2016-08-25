bfbc84059db764b89f68adc8b319b89c1dbb6ba7
toandv <toandv@users.noreply.github.com>
2016-08-25 09:30:56 +0700

    Update HttpMessageWriter.java

--- src/main/java/io/github/toandv/httpserver/HttpMessageWriter.java
+++ src/main/java/io/github/toandv/httpserver/HttpMessageWriter.java
@@ -2,7 +2,7 @@ package io.github.toandv.httpserver;
 
 /**
  * @author toandv
- * @github https://github.com/toandv
+ * @github https://github.com/toandv/http-server
  * @since 8/25/2016.
  */
 public class HttpMessageWriter {
